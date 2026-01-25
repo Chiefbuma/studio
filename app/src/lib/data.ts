de51eefad50af42b3bbdf56c8edb3f6d400dd62f
George Buma <georgebuma.jb@gmail.com>
2026-01-25 15:08:18 +0000

    do we need data ts file now tha we rae fetching from the databse

---
+++ app/src/lib/data.ts
@@ -0,0 +1,3 @@
+// This file previously contained mock data for the prototype.
+// It is no longer in use as the application now fetches data from a real backend API.
+// This file can be safely deleted.
